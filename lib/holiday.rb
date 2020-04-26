cbfd34f9e144e002d884541e9b1b368a883e8bf3
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-26 04:46:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,13 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+  holiday_hash.map do |season, holidays|
+    holidays.map do  |holidays, items|
+    holidays if items.include?("BBQ")
+end 
 end
+.flatten.compact
+end 
 
 
 
